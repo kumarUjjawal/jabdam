9990fd99027e0cfae05fd6a7f07b885aa4ec89d2
Kumar Ujjawal <ujjawalpathak6@gmail.com>
2017-10-22 17:23:19 +0530

    PhotoPicker implementation

--- app/src/main/java/com/usemalloc/jabdam/MainActivity.java
+++ app/src/main/java/com/usemalloc/jabdam/MainActivity.java
@@ -1,12 +1,14 @@
 package com.usemalloc.jabdam;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,7 +20,10 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.*;
+import com.firebase.ui.auth.BuildConfig;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -26,12 +31,22 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_PHOTO_PICKER = 2;
 
     //Request code Firebase UI
     private static final int RC_SIGN_IN = 1;
@@ -44,9 +59,13 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener mChildEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotoStorageReference;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     private static final String ANONYMOUS = "anonymous";
     private static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    private static final String MSG_LENGTH_KEY = "msg_length";
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -67,7 +86,11 @@ public class MainActivity extends AppCompatActivity {
         // Firebase initialization
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
         mMessageDatabaseReference = mFirebaseDatabase.getReference().child("message");
+        mChatPhotoStorageReference = mFirebaseStorage.getReference().child("chat_photos");
 
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mMessageListView = (ListView) findViewById(R.id.messageListView);
@@ -77,15 +100,19 @@ public class MainActivity extends AppCompatActivity {
 
         //message list view and its adapter
         List<MessageModel> messageModelList = new ArrayList<>();
-        mMessageAdapter = new MessageAdapter(this,R.layout.item_message,messageModelList);
+        mMessageAdapter = new MessageAdapter(this, R.layout.item_message, messageModelList);
         mMessageListView.setAdapter(mMessageAdapter);
 
         mProgressBar.setVisibility(ProgressBar.INVISIBLE);
 
+        // Image picker shows image to upload a image for a message
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete Action using"), RC_PHOTO_PICKER);
             }
         });
 
@@ -97,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(charSequence.toString().trim().length() > 0) {
+                if (charSequence.toString().trim().length() > 0) {
                     mSendButton.setEnabled(true);
                 } else {
                     mSendButton.setEnabled(false);
@@ -116,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                MessageModel messageModel = new MessageModel(mMessageEditText.getText().toString(),mUserName,null);
+                MessageModel messageModel = new MessageModel(mMessageEditText.getText().toString(), mUserName, null);
                 mMessageDatabaseReference.push().setValue(messageModel);
                 mMessageEditText.setText("");
             }
@@ -128,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     onSignedInInitialize(user.getDisplayName());
-                    Toast.makeText(MainActivity.this,"You're now signed in.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "You're now signed in.", Toast.LENGTH_SHORT).show();
                 } else {
                     onSignedOutCleanUp();
                     startActivityForResult(
@@ -147,18 +174,37 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
+        //Remote config
+//        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings().Builder()
+//                .setDeveloperModeEnabled(BuildConfig.DEBUG).build();
+//        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+//
+//        Map<String, Object> defaultConfigMap = new HashMap<>();
+//        defaultConfigMap.put(MSG_LENGTH_KEY, DEFAULT_MSG_LENGTH_LIMIT);
+//        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+//        fetchConfig();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             if (resultCode == RESULT_OK) {
-                Toast.makeText(MainActivity.this,"Signed in!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Signed in!", Toast.LENGTH_SHORT).show();
             } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(MainActivity.this,"Signed in canceled!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Signed in canceled!", Toast.LENGTH_SHORT).show();
                 finish();
+            } else if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+                Uri selectedImageUri = data.getData();
+                StorageReference photoRef = mChatPhotoStorageReference.child(selectedImageUri.getLastPathSegment());
+                photoRef.putFile(selectedImageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downloadUri = taskSnapshot.getDownloadUrl();
+                        MessageModel messageModel = new MessageModel(null, mUserName, downloadUri.toString());
+                        mMessageDatabaseReference.push().setValue(messageModel);
+                    }
+                });
             }
         }
     }
@@ -183,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
@@ -248,3 +294,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
